bef7bd04219db80ee402dbe1ed8406d942257a3f
Benji <Benji@Benjamins-MacBook-Pro.local>
2016-02-20 11:39:47 -0500

    signals shit

--- src/signal.c
+++ src/signal.c
@@ -1,8 +1,19 @@
-/*
-	this code sends a message to
-	some emergency service or other
-	third party when ever our app
-	determines that there may be a
-	medical emergency that requires
-	immediate attention
+#include <pebble.h>
+
+/*  this code sends a message to the corresponding android
+	app whenever the pebble determines that there may be
+	a medical emergency that requires immediate attention
 */
+
+static string APIKey = "";
+int main(void){
+	app_message_open();
+		
+
+}
+
+void send_data_to_app(bool fell){
+	Pebble.sendAppMessage({"Fell": fell});
+}
+
+
